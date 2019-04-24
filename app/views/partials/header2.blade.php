4a46b2f1b924a2dad119379fad0aff49049bf0c5
JBPSoft <49833678+JBSSoft@users.noreply.github.com>
2019-04-24 23:04:41 +0200

    Add files via upload

---
+++ app/views/partials/header2.blade.php
@@ -0,0 +1,26 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="icon" type="image/png" href="{{ AdminOptions::base_url()}}/table/assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>JBPSoft</title>
+
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <link href="{{ AdminOptions::base_url()}}/table/assets/css/demo.css" rel="stylesheet" />
+    <link href="{{ AdminOptions::base_url()}}/table/assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="{{ AdminOptions::base_url()}}/table/assets/css/table.css" rel="stylesheet" />
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/bootstrap-table.js"></script>
+
+    <!--  Just for demo purpose, do not include in your project     -->
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/demo/gsdk-switch.js"></script>
+
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/demo/jquery.sharrre.js"></script>
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/table/assets/js/demo/demo.js"></script>
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/js/alertify.js"></script>
+    <script type="text/javascript" src="{{ AdminOptions::base_url()}}/js/admin.js"></script>
